812a3424653fe65e10ac2cdf8ce4aa3f40299f1f
Wildcards7506 <60638589+Wildcards7506@users.noreply.github.com>
2026-04-11 11:30:54 -0500

    Pose hold works (currently X button driver controller) (#15)
    * Reformat robot container
    
    * (WIP) Hold pose with driver X, its drive method is temporarily disabled
    
    * Hold pose w/rotate to hub works yay
    
    ---------
    
    Co-authored-by: 7506wildcards <7506wildcards@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
@@ -29,11 +30,11 @@ import frc.robot.commands.autonomous.AutoRoutines;
 import frc.robot.subsystems.*;
 
 /*
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
- */
+* This class is where the bulk of the robot should be declared.  Since Command-based is a
+* "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+* periodic methods (other than the scheduler calls).  Instead, the structure of the robot
+* (including subsystems, commands, and button mappings) should be declared here.
+*/
 public class RobotContainer {
     // The robot's subsystems
     public final DriveSubsystem drivetrain;
@@ -82,34 +83,54 @@ public class RobotContainer {
                                 controller0.getLeftY(),
                                 controller0.getLeftX(),
                                 controller0.getRightX(),
-                                controller0.getLeftTriggerAxis()  > 0.2 ? 2 :   // FULL BOOST
-                                        controller0.getRightTriggerAxis() > 0.2 ? 1 :   // Full
-                                                0      // Fine (default)
+                                controller0.getLeftTriggerAxis() > 0.2 ? 2 : // FULL BOOST
+                                        controller0.getRightTriggerAxis() > 0.2 ? 1 : // Full
+                                                0 // Fine (default)
                         ), drivetrain));
         controller0.b().onTrue(
                 Commands.runOnce(
                         () -> {
-                                drivetrain.zeroHeading();
-                                Pose2d newBotPose = new Pose2d(drivetrain.getPose().getTranslation(), new Rotation2d(0));
-                                drivetrain.resetOdometry(newBotPose);
-                                Robot.m_field.setRobotPose(newBotPose);
-                        }
-                )
-        );
+                            drivetrain.zeroHeading();
+                            Pose2d newBotPose = new Pose2d(drivetrain.getPose().getTranslation(), new Rotation2d(0));
+                            drivetrain.resetOdometry(newBotPose);
+                            Robot.m_field.setRobotPose(newBotPose);
+                        }));
 
         controller0.rightBumper().whileTrue(
-                superStructure.bringUpRotator()
+                superStructure.bringUpRotator());
+
+        controller0.povUp().whileTrue(
+                Commands.runEnd(
+                        () -> {
+                            drivetrain.alignToTarget(Robot.hubPose);
+                        },
+                        () -> {
+                            drivetrain.drive(0, 0, 0, true);
+                        }, drivetrain)
+
         );
 
-       controller0.povUp().whileTrue(
-            Commands.runEnd(
-                () -> {
-                    drivetrain.alignToTarget(Robot.hubPose);
-                }, 
-                () -> {
-                    drivetrain.drive(0, 0, 0, true);
-                }, drivetrain)
-       
+        controller0.x().whileTrue(
+            Commands.sequence(
+                Commands.runOnce(
+                    () -> {
+                        SmartDashboard.putBoolean("Holding Pose", true);
+                        // drivetrain.testCounter += 1;
+                        // SmartDashboard.putNumber("Test Counter", drivetrain.testCounter);
+                        drivetrain.savePose();
+                    }
+                    ),
+                    Commands.runEnd(
+                        () -> {
+                            drivetrain.alignToTargetHoldPose(Robot.hubPose);
+                        },
+                        () -> {
+                            SmartDashboard.putBoolean("Holding Pose", false);
+                            // drivetrain.testCounter = 0;
+                            // SmartDashboard.putNumber("Test Counter", drivetrain.testCounter);
+                            drivetrain.drive(0, 0, 0, true);
+                        }, drivetrain)
+            )
         );
     }
 
@@ -118,24 +139,21 @@ public class RobotContainer {
         controller1.leftTrigger().whileTrue(
                 superStructure.runIntake()
                         .alongWith(superStructure.runLoader())
-                        .alongWith(superStructure.runIntake2())
-        );
+                        .alongWith(superStructure.runIntake2()));
 
         // Reject Intake2, prevents jamming issue previously encountered
         controller1.rightBumper().whileTrue(
-                superStructure.rejectIntake2()
-        );
+                superStructure.rejectIntake2());
 
         controller1.rightTrigger().whileTrue(
-                Robot.checkAndRunGun(superStructure, false)
-        );
+                Robot.checkAndRunGun(superStructure, false));
         controller1.rightTrigger().onTrue(Commands.runOnce(() -> {
-               Robot.loadingFuel = true;
-                double now = Timer.getFPGATimestamp();
-                if (now - Robot.lastTriggerPressTime < Robot.DOUBLE_PRESS_WINDOW) {
-                        Robot.crippleMode = !Robot.crippleMode;
-                }
-                Robot.lastTriggerPressTime = now;
+            Robot.loadingFuel = true;
+            double now = Timer.getFPGATimestamp();
+            if (now - Robot.lastTriggerPressTime < Robot.DOUBLE_PRESS_WINDOW) {
+                Robot.crippleMode = !Robot.crippleMode;
+            }
+            Robot.lastTriggerPressTime = now;
         }));
         controller1.rightTrigger().onFalse(Commands.runOnce(() -> Robot.loadingFuel = false));
 
@@ -143,65 +161,56 @@ public class RobotContainer {
                 Commands.runOnce(
                         () -> {
                             Robot.useAutoHood = !Robot.useAutoHood;
-                        }
-                )
-        );
+                        }));
 
         controller1.x().whileTrue(
-//       superStructure.primeFlywheel(3025) // rpms lag/drop down to about 2750
-//                superStructure.primeFlywheel(Robot.targetFlywheelRPM)
-//                superStructure.primeFlywheel(4000)
-                superStructure.primeFlywheel(Constants.ShooterConstants.flywheelRPM)
-        );
+                // superStructure.primeFlywheel(3025) // rpms lag/drop down to about 2750
+                // superStructure.primeFlywheel(Robot.targetFlywheelRPM)
+                // superStructure.primeFlywheel(4000)
+                superStructure.primeFlywheel(Constants.ShooterConstants.flywheelRPM));
 
         // Long distance
         controller1.y().whileTrue(
-                superStructure.longDistance()
-        );
+                superStructure.longDistance());
 
         // Mid-distance
         controller1.b().whileTrue(
-                superStructure.midDistance()
-        );
+                superStructure.midDistance());
         // Short distance
         controller1.a().whileTrue(
-                superStructure.shortDistance()
-        );
+                superStructure.shortDistance());
 
         // Dead man button to bring up rotator while button is pressed
         controller1.povUp().whileTrue(
-                superStructure.bringUpRotator()
-        );
-//
+                superStructure.bringUpRotator());
+        //
         controller1.leftBumper().whileTrue(
-                superStructure.rejectIntake()
-        );
+                superStructure.rejectIntake());
 
         controller1.povRight().whileTrue(
-                Agitator.runReject()
-        );
+                Agitator.runReject());
 
-//        controller1.povRight().whileTrue(
-//                Commands.startEnd(
-//                        () -> {
-//                            agitator.setSoftLimitsEnabled(false);
-//                            agitator.runLeft();
-//                            agitator.runRight();
-//                        },
-//                        () -> {
-//                            agitator.setSoftLimitsEnabled(true);
-//                            agitator.stopMotors();
-//                        },
-//                        agitator
-//                )
-//        );
+        // controller1.povRight().whileTrue(
+        // Commands.startEnd(
+        // () -> {
+        // agitator.setSoftLimitsEnabled(false);
+        // agitator.runLeft();
+        // agitator.runRight();
+        // },
+        // () -> {
+        // agitator.setSoftLimitsEnabled(true);
+        // agitator.stopMotors();
+        // },
+        // agitator
+        // )
+        // );
 
-//        controller1.povRight().whileTrue(
-//                agitator.runLeft()
-//        );
-//        controller1.povRight().whileTrue(
-//                agitator.runRight()
-//        );
+        // controller1.povRight().whileTrue(
+        // agitator.runLeft()
+        // );
+        // controller1.povRight().whileTrue(
+        // agitator.runRight()
+        // );
     }
 
     /**
